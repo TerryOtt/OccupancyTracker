d92a30285aabd6ad6212122b4c6c690515f2711a
Terry Ott <Terry.Ott@globalwatchpost.com>
2020-05-22 02:56:26 +0000

    Now can create a room with a set occupancy

--- occupancy-tracker/handler.py
+++ occupancy-tracker/handler.py
@@ -53,6 +53,7 @@ def _createOccupancyResponse( statusCode, spaceId, current, maximum, created, la
 
 def create_space(event, context):
     try:
+        currOccupancy = int( event['pathParameters']['current_occupancy'] )
         maxOccupancy = int( event['pathParameters']['max_occupancy'] )
     except:
         logger.error("Could not parse {0} as integer".format( event['pathParameters']['max_occupancy']) )
@@ -63,6 +64,8 @@ def create_space(event, context):
     if maxOccupancy < 1 or maxOccupancy > 100000:
         return _createHandlerResponse( 400, 
             "Invalid occupancy value: {0}, passed value must be 1 <= max_occupancy <= 100000".format(maxOccupancy) )
+    if currOccupancy > maxOccupancy:
+        currOccupancy = maxOccupancy
 
     newRoomId = uuid.uuid4() 
 
@@ -78,7 +81,7 @@ def create_space(event, context):
             Item            = {
                 'PK'            : { 'S': str(newRoomId) },
                 'occupancy'     : { 'M': {
-                        'current_occupancy': { 'N': str(0), },
+                        'current_occupancy': { 'N': str(currOccupancy) },
                         'maximum_occupancy': { 'N': str(maxOccupancy) } 
                     }
                 },
@@ -94,10 +97,16 @@ def create_space(event, context):
     except botocore.exceptions.ClientError as e:
         logger.error("Could not add new values to Dynamo: {0}".format(e) )
         return _createHandlerResponse( 500, "DB write fail" )
+    except Exception as e:
+        logger.error("threw non-botocore exception during DB put: {0}".format(e))
+        return  _createHandlerResponse( 500, "DB write fail" )
+    except:
+        logger.error("Threw totally alien exception during DB put" )
+        return  _createHandlerResponse( 500, "DB write fail" )
 
     logger.info("Created new space {0} w/ max occupancy {1}".format(newRoomId, maxOccupancy) )
 
-    return _createOccupancyResponse( 200, newRoomId, 0, maxOccupancy, nowTimestamp, nowTimestamp )
+    return _createOccupancyResponse( 200, newRoomId, currOccupancy, maxOccupancy, nowTimestamp, nowTimestamp )
 
 
 def get_occupancy(event, context):
@@ -114,6 +123,13 @@ def get_occupancy(event, context):
     except botocore.exceptions.ClientError as e:
         logger.error("Could not read DB stats for space {0}: {1}".format(spaceId, e) )
         return _createHandlerResponse( 500, "DB read fail for space {0}".format(spaceId) )
+    except Exception as e:
+        logger.error("threw non-botocore exception during DB get: {0}".format(e))
+        return  _createHandlerResponse( 500, "DB read fail for space {0}".format(spaceId) )
+    except:
+        logger.error("Threw totally alien exception during DB read" )
+        return  _createHandlerResponse( 500, "DB read fail for space {0}".format(spaceId) )
+
 
     if 'Item' not in occupancyInfo:
         return _createHandlerResponse( 400, 
@@ -159,8 +175,15 @@ def increment( event, content ):
     except botocore.exceptions.ClientError as e:
         logger.error("Could not increment occupancy for space {0}: {1}".format(spaceId, e) )
         return _createHandlerResponse( 500, "DB increment fail for space {0}".format(spaceId) )
+    except Exception as e:
+        logger.error("threw non-botocore exception during occupancy increment: {0}".format(e))
+        return  _createHandlerResponse( 500, "DB increment fail for space {0}".format(spaceId) )
+    except:
+        logger.error("Threw totally alien exception during DB put" )
+        return  _createHandlerResponse( 500, "DB increment fail for space {0}".format(spaceId) )
 
-    logger.info( json.dumps(occupancyInfo, indent=4, sort_keys=True) )
+
+    logger.debug( json.dumps(occupancyInfo, indent=4, sort_keys=True) )
 
     return _createOccupancyResponse( 200, spaceId, 
         occupancyInfo['Attributes']['occupancy']['M']['current_occupancy']['N'],
@@ -198,11 +221,73 @@ def decrement( event, context ):
     except botocore.exceptions.ClientError as e:
         logger.error("Could not decrement occupancy for space {0}: {1}".format(spaceId, e) )
         return _createHandlerResponse( 500, "DB decrement fail for space {0}".format(spaceId) )
+    except Exception as e:
+        logger.error("threw non-botocore exception during occupancy decrement: {0}".format(e))
+        return  _createHandlerResponse( 500, "DB decrement fail" )
+    except:
+        logger.error("Threw totally alien exception during DB put" )
+        return  _createHandlerResponse( 500, "DB decrement fail" )
 
-
-    logger.info( json.dumps(occupancyInfo, indent=4, sort_keys=True) )
+    logger.debug( json.dumps(occupancyInfo, indent=4, sort_keys=True) )
 
     return _createOccupancyResponse( 200, spaceId, 
+        occupancyInfo['Attributes']['occupancy']['M']['current_occupancy']['N'],
+        occupancyInfo['Attributes']['occupancy']['M']['maximum_occupancy']['N'],
+        occupancyInfo['Attributes']['created']['S'],
+        occupancyInfo['Attributes']['last_updated']['S']
+    )
+
+
+def change_max_occupancy( event, context ):
+    spaceId             = event['pathParameters']['space_id']
+    try:
+        newMaxOccupancy = int(event['pathParameters']['new_max_occupancy'])
+    except Exception as e:
+        logger.error("Could not parse {0} as integer".format( event['pathParameters']['new_max_occupancy']) )
+        return _createHandlerResponse( 400, "Passed new max occupancy value of {0} cannot be parsed as an integer".format(
+            event['pathParameters']['new_max_occupancy']) )
+
+
+    # Sanity check max occupancy
+    if newMaxOccupancy < 1 or newMaxOccupancy > 100000:
+        return _createHandlerResponse( 400,
+            "Invalid max occupancy value: {0}, passed value must be 1 <= max_occupancy <= 100000".format(newMaxOccupancy) )
+
+    try:
+        nowTimestamp = "{0}Z".format(datetime.datetime.utcnow().isoformat())
+        expirationSecondsSinceEpoch = time.time() + ttlSeconds
+
+        occupancyInfo = daxHandle.update_item(
+            TableName       = table_name,
+            Key             = {
+                'PK': { 'S': str(spaceId) }
+            },
+            UpdateExpression            = "SET occupancy.maximum_occupancy = :new_maximum_occupancy, " + \
+                "last_updated = :updated_now, " + \
+                "TTL = :ttl",
+
+            ExpressionAttributeValues   = {
+                ':new_maximum_occupancy'    : { 'N': str(newMaxOccupancy) },
+                ':updated_now'              : { 'S': nowTimestamp },
+                ':ttl'                      : { 'N': str(expirationSecondsSinceEpoch) }
+            },
+
+            ReturnValues="ALL_NEW"
+        )
+
+    except botocore.exceptions.ClientError as e:
+        logger.error("Could not set new max occupancy for space {0}: {1}".format(spaceId, e) )
+        return _createHandlerResponse( 500, "DB occupancy change fail for space {0}".format(spaceId) )
+    except Exception as e:
+        logger.error("threw non-botocore exception during occupancy change: {0}".format(e))
+        return  _createHandlerResponse( 500, "Occupancy change fail" )
+    except:
+        logger.error("Threw totally alien exception during DB put" )
+        return  _createHandlerResponse( 500, "Occupancy change fail" )
+
+    logger.debug( json.dumps(occupancyInfo, indent=4, sort_keys=True) )
+
+    return _createOccupancyResponse( 200, spaceId,
         occupancyInfo['Attributes']['occupancy']['M']['current_occupancy']['N'],
         occupancyInfo['Attributes']['occupancy']['M']['maximum_occupancy']['N'],
         occupancyInfo['Attributes']['created']['S'],
